9383daf9e6f3d13fdd85e7e36784c4dd032708bb
lukasnee <lukas.neverauskis@gmail.com>
2022-04-24 04:07:29 +0300

    upd: first working version!

--- install.py
+++ install.py
@@ -1,16 +1,17 @@
 # kicad 5.x library installer from official repositories.
+from pathlib import Path
+import re
+import sys
+import os
 
+test = False
 # HELPERS ######################################################################
 
-import os
-import sys
-
-from pathlib import Path
 
 class bcolors:
     HEADER = '\033[95m'
     BLUE = '\033[94m'
-    OKBLUE = '\033[94m'
+    YELLOW = '\033[93m'
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
@@ -20,15 +21,19 @@ class bcolors:
     WHITE = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def printc(bcolor, text, end='\n'):
     print(bcolor + text + bcolors.ENDC, end=end)
-    
+
+
 def system(cmd):
     printc(bcolors.OKCYAN, cmd)
     return (0 == os.system(cmd))
 
-def sys_exit_error(text = ""):
+
+def sys_exit_error(text=""):
     sys.exit(bcolors.FAIL + "error: " + text + bcolors.ENDC)
+
 
 def cd(newdir):
     prevdir = os.getcwd()
@@ -37,13 +42,23 @@ def cd(newdir):
         yield
     finally:
         os.chdir(prevdir)
-        
+
+
 class Lib():
     def __init__(self, dir, listfn):
         self.dir = dir
         self.listfn = listfn
 
+
+def return_first_match(regex, text):
+    try:
+        result = re.findall(regex, text)[0]
+    except IndexError:
+        result = ''
+    return result
+
 # CONFIG #######################################################################
+
 
 libs = [
     Lib("kicad-footprints", "fp-lib-table"),
@@ -54,6 +69,8 @@ libs = [
 
 # SCRIPT #######################################################################
 kiCadDir = str(Path.home()) + "\AppData\Roaming\kicad"
+if test:
+    kiCadDir = "..\\tests"
 
 try:
     if not system("git submodule update --init --recursive"):
@@ -62,7 +79,7 @@ try:
     for lib in libs:
         prevdir = os.getcwd()
         os.chdir(lib.dir)
-        printc(bcolors.OKBLUE, "opening ", end ="")
+        printc(bcolors.YELLOW, "updating %s manager from" % lib.dir, end=" ")
         printc(bcolors.WHITE, os.getcwd())
         if not system("git checkout master"):
             sys_exit_error()
@@ -70,21 +87,31 @@ try:
             sys_exit_error()
         else:
             with open(lib.listfn, 'r') as srcf:
-                printc(bcolors.OKBLUE, os.path.realpath(srcf.name))
-                # print(srcf.read())
-                srcfLines = srcf.readlines()
-                for srcfLinesl in srcfLines:
-                    pass
-                with open(kiCadDir + "\\" + lib.listfn) as dstf:
-                    printc(bcolors.OKBLUE, os.path.realpath(dstf.name))
-                    # print(dstf.read())
-                    dstfLines = dstf.readlines()
-                    for dstfLinesl in dstfLines:
-                        pass
-                    
+                printc(bcolors.YELLOW, "src:", end=" ")
+                printc(bcolors.WHITE, os.path.realpath(srcf.name))
+                srclines = srcf.readlines()
+                dstPath = kiCadDir + "\\" + lib.listfn
+                with open(dstPath, 'r') as dstf:
+                    printc(bcolors.YELLOW, "dst:", end=" ")
+                    printc(bcolors.WHITE, os.path.realpath(dstf.name))
+                    dstlines = dstf.readlines()
+                    for srcline in srclines:
+                        regexSrc = '\(name "?(\w+)"?\)*'
+                        libName = return_first_match(regexSrc, srcline)
+                        if libName:
+                            regexDst = '\(name "?(' + libName + ')"?\)*'
+                            isAlreadyPresent = False
+                            for dstline in dstlines:
+                                if bool(re.search(regexDst, dstline)):
+                                    isAlreadyPresent = True
+                            if not isAlreadyPresent:
+                                dstlines.insert(1, srcline)
+                    with open(dstPath, "w") as f:
+                        dstlines = "".join(dstlines)
+                        f.write(dstlines)
         os.chdir(prevdir)
 
-except Exception as e: 
+except Exception as e:
     print(e)
     sys_exit_error()
 
