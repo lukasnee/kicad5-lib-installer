72b1a3cf4ddde4582254e63ad05c99b96bab60dc
lukasnee <lukas.neverauskis@gmail.com>
2022-05-07 18:39:08 +0300

    upd: add support for KiCad v6

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Kilibup
 
-Script tool that downloads and links all [official KiCad libraries](https://gitlab.com/kicad/libraries) to your local KiCad installation (library configuration files `*-lib-table`). ATM only the schematic symbol and PCB footprint libararies installation are supported.
+Script tool that downloads and links all [official KiCad libraries](https://gitlab.com/kicad/libraries) to your local KiCad v6 or v5 installation (library configuration files `*-lib-table`). ATM only the schematic symbol and PCB footprint libararies installation are supported.
 
 ## Motivation
 I found it very tedious to do this manually using library manager GUI and I couldn't find any help on the internet to speed up this process. So I made this tool. Cheers! 
@@ -9,30 +9,31 @@ PS: It's my very first python application. lol.
 
 # Dependencies
 
-- [KiCad](https://www.kicad.org/) v5
+- [KiCad](https://www.kicad.org/) v6 or v5
 - [git](https://git-scm.com/)
 - [python](https://www.python.org/downloads/) v3.8.10 or higher
 
 # Usage
 
-```
-python kilibup.py -h
-usage: kilibup.py [-h] [--sym sym-lib-table] [--fp fp-lib-table] [-s]
+0. Open up powershell or shell on this repository directory.
 
-kiCad v5 official library unofficial installer
+1. See help, familiarize with options.
+    ```
+    python kilibup.py -h
+    ```
 
-optional arguments:
-  -h, --help           show this help message and exit
-  --sym sym-lib-table  path to sym-lib-table (KiCad schematic symbols library manager file)
-  --fp fp-lib-table    path to fp-lib-table (KiCad PCB footprint library manager file)
-  -s                   do not update libraries from remote repository. use local.
+1. Run the script:
 
-example test: python kilibup.py --sym ./tests/sym-lib-table --fp ./tests/fp-lib-table
-```
+    for KiCad v6:
+    ```
+    python kilibup.py
+    ```
 
-## Example
+    for KiCad v5:
+    ```
+    python kilibup.py -v 5
+    ```
 
-Windows:
-```
-python .\kilibup.py --sym C:\Users\Lukas\AppData\Roaming\kicad\sym-lib-table --fp C:\Users\Lukas\AppData\Roaming\kicad\fp-lib-table
-```
+2. Restart KiCad
+3. Open up KiCad library managers to see if no errors and new libraries added.
+4. Cheers!
--- kilibup.py
+++ kilibup.py
@@ -18,8 +18,7 @@ def print_colored(color, text, end='\n'):
 
 
 def system_call(cmd):
-    print_colored(Fore.YELLOW, "invoking:", end=" ")
-    print_colored(Fore.WHITE, cmd)
+    print("invoking '{cmd}'".format(cmd=cmd))
     if os.system(cmd) != 0:
         raise Exception('{cmd} failed executing'.format(cmd))
 
@@ -48,29 +47,43 @@ class Lib():
         git = "git --git-dir={dir}/.git --work-tree={dir} ".format(
             dir=os.path.dirname(self.src_path))
         sys_script = [
+            "git submodule update {submodule}".format(
+                submodule=os.path.dirname(self.src_path)),
             git + "fetch --all",
             git + "checkout master",
             git + "clean -fdx",
             git + "fetch",
             git + "pull"
         ]
-        print_colored(Fore.YELLOW, "updating %s manager from" %
-                      self.src_path, end=" ")
-        print_colored(Fore.WHITE, os.getcwd())
         exec_sys_script(sys_script)
 
     # def update_library_manager:
 
 
 parser = argparse.ArgumentParser(
-    description='kiCad v5 official library unofficial installer', epilog="example test: python kilibup.py --sym ./tests/sym-lib-table --fp ./tests/fp-lib-table")
+    description='KiCad v6 (or v5) official libraries update script', epilog="example test: git restore tests/*; python kilibup.py --sym ./tests/sym-lib-table --fp ./tests/fp-lib-table")
+parser.add_argument('-v', type=int, choices=[5, 6], default=6,
+                    help='Your KiCad version to update. Version 6 by default')
 parser.add_argument('--sym', type=str,
-                    metavar="sym-lib-table", help='path to sym-lib-table (KiCad schematic symbols library manager file)')
+                    metavar="sym-lib-table", required=False, help='custom path to sym-lib-table (KiCad schematic symbols library manager file). If used, -v flag can be ingored')
 parser.add_argument('--fp', type=str,
-                    metavar="fp-lib-table", help='path to fp-lib-table (KiCad PCB footprint library manager file)')
+                    metavar="fp-lib-table", required=False, help='custom path to fp-lib-table (KiCad PCB footprint library manager file). If used, -v flag can be ingored')
 parser.add_argument('-s', action='store_true',
-                    help='do not update libraries from remote repository. use local.')
+                    help='do not update libraries repositories from remote')
 args = parser.parse_args()
+
+if not args.sym:
+    if args.v == 5:
+        args.sym = str(Path.home()) + "/AppData/Roaming/kicad/sym-lib-table"
+    elif args.v == 6:
+        args.sym = str(Path.home()) + \
+            "/AppData/Roaming/kicad/6.0/sym-lib-table"
+
+if not args.fp:
+    if args.v == 5:
+        args.fp = str(Path.home()) + "/AppData/Roaming/kicad/fp-lib-table"
+    elif args.v == 6:
+        args.fp = str(Path.home()) + "/AppData/Roaming/kicad/6.0/fp-lib-table"
 
 libs = [
     Lib("PCB footprints", "kicad-footprints/fp-lib-table",
@@ -87,33 +100,32 @@ for lib in libs:
 
     if lib.dst_path:
 
-        if not libs_updated_cnt:  # first time once
-            system_call("git submodule update --init --recursive")
-
-        if not args.s:
-            lib.git_reset_latest_master()
-
         print_colored(Fore.YELLOW, "updating {lib_name} library manager".format(
             lib_name=os.path.basename(lib.name)))
 
-        backup_path = lib.src_path + ".bak"
-        print("backing up {backup_path}".format(backup_path=backup_path))
-        shutil.copyfile(lib.src_path, backup_path)
+        if not args.s:
+            print_colored(
+                Fore.YELLOW, "making sure you have latest library repository...")
+            lib.git_reset_latest_master()
+
+        backup_path = lib.dst_path + "." + \
+            os.path.basename(sys.argv[0]) + ".bak"
+        print_colored(Fore.YELLOW, "backing up {backup_path}".format(
+            backup_path=backup_path))
+        shutil.copyfile(lib.dst_path, lib.dst_path + ".bak")
+
         with open(lib.src_path, 'r') as f:
-            print_colored(Fore.YELLOW, "src_file:", end=" ")
-            print_colored(Fore.WHITE, lib.src_path)
+            print_colored(Fore.YELLOW, "src_file: " + lib.src_path)
             src_lines = f.readlines()
 
-        backup_path = lib.dst_path + ".bak"
-        print("backing up {backup_path}".format(backup_path=backup_path))
-        shutil.copyfile(lib.dst_path, lib.dst_path + ".bak")
         with open(lib.dst_path, 'r') as f:
-            print_colored(Fore.YELLOW, "dst_file:", end=" ")
-            print_colored(Fore.WHITE, lib.dst_path)
+            print_colored(Fore.YELLOW, "dst_file: " + lib.dst_path)
             dst_lines = f.readlines()
 
         updated_cnt = 0
         added_cnt = 0
+
+        print_colored(Fore.YELLOW, "scanning libraries:")
 
         for src_line in src_lines:
             re_name = r'\(\s*name\s+"?(?P<name>[\w\-\.\+]+)"?\s*\)'
@@ -141,12 +153,15 @@ for lib in libs:
         with open(lib.dst_path, "w") as f:
             dst_lines = "".join(dst_lines)
             f.write(dst_lines)
-        print("\n{added_cnt} libraries added and {updated_cnt} libraries updated".format(
+        print_colored(Fore.YELLOW, "\n{added_cnt} libraries added and {updated_cnt} libraries updated".format(
             added_cnt=added_cnt, updated_cnt=updated_cnt))
 
         libs_updated_cnt += 1
 
 if not libs_updated_cnt:
-    print("nothing to update. Please provide destination map file(s) - see help with '-h'")
+    print_colored(
+        Fore.YELLOW, "nothing to update. Please provide destination map file(s) - see help with '-h'")
 else:
-    print('all done!')
+    print_colored(
+        Fore.YELLOW, 'NOTE: do not delete kilibup repo because it contains the actual libraries that are linked to your KiCad')
+    print_colored(Fore.GREEN, 'all done!')
